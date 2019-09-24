e9333fec7aa724af8c5d0957b61e0970e419d01d
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-24 15:32:53 +0300

    Update N4.cpp

--- N4.cpp
+++ N4.cpp
@@ -6,7 +6,7 @@ int main() {
 	setlocale(0,"");
 	int a, b;
 	cin >> a;
-	b = (a - a / 100 * 100 - a % 10)/10;
+	b = (a % 100 - a % 10) / 10;
 	if (a > 99 && a < 1000 && (a / 100) < b && b < a % 10) cout << "True\n";
 	else cout << "False\n";
 	system("pause");
