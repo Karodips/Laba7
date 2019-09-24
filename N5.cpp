b09ee5cd5d97b6d237e2158b96a4ea8711e6912f
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-24 15:41:16 +0300

    Update N5.cpp

--- N5.cpp
+++ N5.cpp
@@ -6,7 +6,7 @@ int main() {
 	setlocale(0,"");
 	int a, b;
 	cin >> a;
-	b = (a - a % 1000) / 1000 + 10 * ((a % 1000 - a % 100) / 100) + 100 * ((a % 100 - a % 10) / 10) + a % 10 * 1000;
+	b = (a - a % 1000) / 1000 + (a % 1000 - a % 100)/10 + 10*(a % 100 - a % 10) + a % 10 * 1000;
 	if (a == b && a > 999 && a < 10000) cout << "True\n";
 	else cout << "False\n";
 	system("pause");
